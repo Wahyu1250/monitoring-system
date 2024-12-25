5d0c8aa2068099ff0a36c3db2779cc2eb549d1d3
Wahyu Nr <51579699+Wahyu1250@users.noreply.github.com>
2024-12-25 21:36:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const firebaseConfig = {
   measurementId: "G-GWC34GC7LP"
 };
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service worker registered:', registration);
+      })
+      .catch(error => {
+        console.error('Service worker registration failed:', error);
+      });
+  });
+}
+
 // Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
